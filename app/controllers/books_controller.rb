979b456fd5141c8423cc437da9129cff0229f669
SS0117 <103245744+SS0117@users.noreply.github.com>
2023-02-27 20:56:11 +0000

    [Fix]

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,62 @@
+class BooksController < ApplicationController
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    # @book1 = Book.new(book_params)
+  end
+
+  def update
+    @book = Book.new(book_params)
+    # @book_error = Book.new(book_params)
+    if @book.valid?
+      @book = Book.find(params[:id])
+      @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      # @books = Book.all
+      # @book = Book.find(params[:id])
+      # render :edit
+      # @book = Book.find(params[:id])
+      @book.update(book_params)
+      render :edit
+    end
+
+    # book = Book.find(params[:id])
+    # book.update(book_params)
+    # flash[:notice] = "Book was successfully updated."
+    # redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to '/books'
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
